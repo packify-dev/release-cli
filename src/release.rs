4fc1b881e14620402b312e8c210add4f8fbb8f77
Leonard Lesinski <84378319+Le0X8@users.noreply.github.com>
2024-12-23 17:22:37 +0100

    Added code that was missing for some reason

--- src/release.rs
+++ src/release.rs
@@ -291,4 +291,28 @@ pub fn release(r#type: String) {
         .unwrap()
         .wait()
         .unwrap();
+
+    Command::new("gh")
+        .args([
+            "release",
+            "create",
+            format!("v{}", ver).as_str(),
+            "--generate-notes",
+            "--target",
+            match new_channel {
+                ReleaseType::Alpha(_) => "alpha",
+                ReleaseType::Beta(_) => "beta",
+                ReleaseType::Candidate(_) => "rc",
+                ReleaseType::Stable => "main",
+            },
+            "--latest",
+            match new_channel {
+                ReleaseType::Stable => "",
+                _ => "--prerelease",
+            },
+        ])
+        .spawn()
+        .unwrap()
+        .wait()
+        .unwrap();
 }
